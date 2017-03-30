6d8a9784e588d9154911e304d0dd014963d36c03
Max Rothman <max@edx.org>
2017-03-30 15:55:08 -0400

    check full bodies in ES verify script

--- util/elasticsearch/verify-index-copy.py
+++ util/elasticsearch/verify-index-copy.py
@@ -5,6 +5,7 @@ Verifies that an index was correctly copied from one ES host to another.
 import itertools
 import pprint
 import random
+import json
 
 from deepdiff import DeepDiff
 from elasticsearch import Elasticsearch
@@ -58,19 +59,92 @@ def grouper(iterable, n):
     return itertools.izip_longest(*args)
 
 
-def find_matching_ids(es, index, ids, bodies):
+def docs_match(old_doc, new_doc):
+    """
+    Return True if the the docs match, minus the ignorable fields
+
+    Args:
+        old_doc: a dict of an elasticsearch doc from the old cluster
+        new_doc: a dict of an elasticsearch doc from the new cluster
+    """
+    """
+    example doc:
+    {'dictionary_item_added': {
+        "root['_source']['_id']",
+        "root['_source']['abuse_flaggers']",
+        "root['_source']['anonymous']",
+        "root['_source']['anonymous_to_peers']",
+        "root['_source']['at_position_list']",
+        "root['_source']['author_username']",
+        "root['_source']['closed']",
+        "root['_source']['comment_count']",
+        "root['_source']['historical_abuse_flaggers']",
+        "root['_source']['pinned']",
+        "root['_source']['thread_type']",
+        "root['_source']['visible']",
+        "root['_source']['votes']",
+        "root['found']"},
+     'dictionary_item_removed': {
+        "root['_source']['id']",
+        "root['_source']['thread_id']",
+        "root['_source']['votes_point']",
+        "root['exists']"},
+     'values_changed': {
+        "root['_index']": {
+            'new_value': u'content_20170324145539907',
+            'old_value': u'content_20151207225034'},
+        "root['_source']['body']": {
+            'new_value': u'encryption neglect hypothesize polluters wining pitiably prophetess apostrophe foretelling assignments diaphragms trustees scroll scruffs shrivels characterizes digraph lasted sharked rewind chamoix charier protoplasm rapports isolated upbraid mortgaged cuddled indefinitely sinful insaner slenderized cemetery deject soundly preventable',
+            'old_value': u'embellishing orbitals complying alternation welching sepulchered grate blench placenta landslide dependance hurdle predicted chaplet earsplitting assess awol necrosis freeways skipper delicatessen sponsorship bellboys antiseptics gabardines admittedly screechier professional roughness educations nutting valences iridescence deductions'},
+        "root['_source']['title']": {
+            'new_value': u'southpaw afterward playgoers roughed requites arrived byplay ninetieth textural rental foreclosing',
+            'old_value': u'guttersnipes corduroys ghostly discourtesies'},
+        "root['_source']['updated_at']": {
+            'new_value': u'2017-03-29T18:51:19Z',
+            'old_value': u'2017-03-28T12:58:02Z'},
+        "root['_version']": {
+            'new_value': 20,
+            'old_value': 1}}}
+"""
+    ignorable_fields = [
+        "root['exists']",
+        "root['found']",
+        "root['_index']",
+        "root['updated_at']",
+        "root['version']",
+    ]
+    diff_types = ['dictionary_item_added', 'dictionary_item_removed', 'values_changed']
+    diff_doc = DeepDiff(old_doc, new_doc)
+
+    #if this fails something is horribly wrong
+    if set(diff_doc.keys()) != set(diff_types):
+        print 'ERROR: expected to be diffing dictionaries, got something else! id: {}'.format(
+            new_doc['_id'])
+
+    for diff_type in diff_types:
+        for field in ignorable_fields:
+            #values_changed is a set, the other two are dicts
+            if isinstance(diff_doc[diff_type], set):
+                diff_doc[diff_type].discard(field)
+            else:
+                diff_doc[diff_type].pop(field, None)
+
+    return all(len(diff_doc[diff_type]) == 0 for diff_type in diff_types)
+
+
+def find_matching_ids(es, index, ids, docs):
     """
     Finds out how many of the ids in the given ids are in the given index in the given
     ES deployment.
 
-    We also compare the bodies of the documents to ensure that those still match.
-    No other metadata is checked.
+    We also compare documents to ensure that those still match, skipping a few fields
+    (see docs_match() for which ones).
 
     Args:
         es - Elasticsearch instance corresponding to the cluster we want to check
         index - name of the index that we want to check
         ids - a list of dictionaries of the form {'_id': <id>} of the ids we want to check.
-        bodies - a dictionary of the form {'<id>': '<text body>'}
+        docs - a dictionary of the form {'<id>': document}, where "document"s are full ES docs
     """
     body = {'docs': ids}
 
@@ -81,18 +155,15 @@ def find_matching_ids(es, index, ids, bodies):
         # 'exists' is 0.9.x
         # 'found' is 1.5.x
         if elt.get('exists', False) or elt.get('found', False):
-            if elt['_source']['body'] == bodies[elt['_id']]:
+            if docs_match(docs[elt['_id']], elt):
                 matching += 1
             else:
-                print 'ERROR: Document with id {id} does not match body: {body}'.format(
-                    id=elt['_id'], body=bodies[elt['_id']]
-                )
-                print 'ERROR: Found body: {body}'.format(
-                    body=elt['_source']['body']
-                )
+                print 'FAILURE: Documents with id {id} do not match: '.format(
+                    id=elt['_id']
+                ) + json.dumps({'diff': DeepDiff(docs[elt['_id']], elt), 'new': elt, 'old': docs[elt['_id']]})
         else:
-            print 'ERROR: Document missing with id: {id}, body: {body}'.format(
-                id=elt['_id'], body=bodies[elt['_id']]
+            print 'ERROR: Document with id {id} missing: {doc}'.format(
+                id=elt['_id'], doc=docs[elt['_id']]
             )
     return matching
 
@@ -113,13 +184,13 @@ def scan_documents(old_es, new_es, old_index, new_index):
     for old_elts in grouper(old_iter, SCAN_ITER_STEP):
 
         old_elt_ids = []
-        old_elt_bodies = {}
+        old_elt_docs = {}
         for elt in old_elts:
             if elt is not None:
                 old_elt_ids.append({'_id': elt['_id']})
-                old_elt_bodies[elt['_id']] = elt['_source']['body']
+                old_elt_docs[elt['_id']] = elt
 
-        matching += find_matching_ids(new_es, new_index, old_elt_ids, old_elt_bodies)
+        matching += find_matching_ids(new_es, new_index, old_elt_ids, old_elt_docs)
         total += len(old_elt_ids)
         if total % 100 == 0:
             print 'processed {} items'.format(total)
@@ -163,11 +234,11 @@ def random_checks(old_es, new_es, old_index, new_index, total_document_count, ch
             index=old_index, body=body
         )
         ids = []
-        bodies = {}
+        docs = {}
         for elt in results['hits']['hits']:
             ids.append({'_id': elt['_id']})
-            bodies[elt['_id']] = elt['_source']['body']
-        matching += find_matching_ids(new_es, new_index, ids, bodies)
+            docs[elt['_id']] = elt
+        matching += find_matching_ids(new_es, new_index, ids, docs)
         num_elts = len(ids)
         total += num_elts
         current_offset += num_elts
